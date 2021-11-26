4ea1857544f6a97f59c61eca8a9795cf3254a5a1
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2021-11-26 18:46:09 +0100

    feat(handler): Add new handler manager for handling request

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import { Servient } from "@node-wot/core"
 import { HttpServer } from "@node-wot/binding-http"
 import { logger } from "./utils/logger"
 import { smartCmTd } from "./utils/things-descriptors"
+import {HandlerManager} from "./handlers/HandlerManager"
+import {CoffeeMachine, CoffeeMachineImpl} from "./model/coffee-machine"
 
 const thingServer = new Servient()
 thingServer.addServer(
@@ -14,8 +16,10 @@ thingServer.addServer(
     logger.info("Starting thing")
     const wot = await thingServer.start()
     const thing = await wot.produce(smartCmTd)
-    thing.setActionHandler("deliver", async (params, ) => {
-        logger.info("params: " + JSON.stringify(params))
-    })
+    const coffeeMachine: CoffeeMachine = new CoffeeMachineImpl([])
+
+    const handlerManager = new HandlerManager(thing, coffeeMachine)
+
+    thing.setActionHandler("deliver", handlerManager.deliverActions)
     await thing.expose()
 })()
